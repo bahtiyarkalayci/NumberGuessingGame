c50c73ec3fe1a28f6d3ff5a85aa71c6cae09286d
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-30 19:24:53 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random=new Random();
+        int number=random.nextInt(100);
+        System.out.println(number);
+        int right=0;
+        int[] wrong=new int[5];
+
+
+        while (right<5){
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("lütfen 0-100 arasında tahmininizi giriniz");
+            int estimate=scanner.nextInt();
+            if (estimate==number){
+                System.out.println("tebrikler doğru tahmin ettiniz, tahmininiz:"+number);
+                break;
+
+            }else {
+                if (estimate<number){
+                    System.out.println("küçük bir sayı, daha büyük dene :)");
+                }else {
+                    System.out.println("büyük bir sayı,daha küçüğünü dene:)");
+
+                }
+
+               wrong[right]=estimate;
+                right++;
+                System.out.println("kalan hakkınız:"+(5-right));
+
+            }
+
+
+
+        }
+        System.out.println("hatalarınız:"+ Arrays.toString(wrong));
+
     }
 }
